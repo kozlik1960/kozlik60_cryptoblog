47965c87fdddecfae09079a9b120ce106954c158
kozlik1960 <kozlik60@post.ru>
2018-10-13 09:45:37 +0300

    back_1

--- wp-content/plugins/tokenad/public/js/token-ad-public.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    var element=document.getElementsByClassName('header_fix_top');
-    if(element[0]){
-		document.body.style.marginTop = '80px';
-		window.scrollBy(0, -100);
-    }
-};
